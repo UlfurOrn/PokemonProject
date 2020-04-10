8fd42b9d98fa5c1772227c08545e0c8687d42df8
Ulfur_Orn <ulfurinn@gmail.com>
2020-04-10 15:32:49 +0000

    Player is now visible on screen

--- Game.py
+++ Game.py
@@ -1,14 +1,17 @@
 import pygame
 
+from Player import Player
+
 class Game:
     SIZE = (640, 480)
     FRAME_RATE = 60
 
     def __init__(self):
-        self.setup()
+        self.setup_pygame()
+        self.setup_sprites()
 
 
-    def setup(self):
+    def setup_pygame(self):
         pygame.init()
         
         self.screen = pygame.display.set_mode(self.SIZE)
@@ -17,20 +20,33 @@ class Game:
         self.clock = pygame.time.Clock()
     
 
+    def setup_sprites(self):
+        self.sprite_list = pygame.sprite.Group()
+
+        self.sprite_list.add(Player())
+    
+
     def start(self):
         self.do_loop = True
 
         while self.do_loop:
 
             self.handle_events()
+            self.update()
+
+            self.sprite_list.draw(self.screen)
+
             pygame.display.flip()
 
+            # Set framerate to 60
             self.clock.tick(self.FRAME_RATE)
     
 
+    def update(self):
+        self.sprite_list.update()
+
 
     def handle_events(self):
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.do_loop = False
--- Player.py
+++ Player.py
@@ -1,6 +1,17 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
+    IMAGE_SIZE = [128, 128]
 
     def __init__(self):
-        super().init()
+        super().__init__()
+
+        # Load and resize image to IMAGE_SIZE
+        self.image = pygame.image.load("Sprite_Sheet/Stand/Stand_S.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, self.IMAGE_SIZE)
+
+        # Create rect variable for draw call in Game loop
+        self.rect = self.image.get_rect()
+
+
+    def
